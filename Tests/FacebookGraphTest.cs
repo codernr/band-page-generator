5b2ce646767149d0b4e5410e4f11ad27133df4d9
csorez <csore.zoltan@gmail.com>
2019-01-28 20:52:47 +0100

    #1 Add simple test to FacebookGraph

--- Tests/FacebookGraphTest.cs
+++ Tests/FacebookGraphTest.cs
@@ -1,4 +1,9 @@
-using System;
+using BandPageGenerator.Config;
+using BandPageGenerator.Models;
+using BandPageGenerator.Services;
+using BandPageGenerator.Services.Interfaces;
+using Microsoft.Extensions.Options;
+using Moq;
 using Xunit;
 
 namespace Tests
@@ -6,9 +11,20 @@ namespace Tests
     public class FacebookGraphTest
     {
         [Fact]
-        public void Test1()
+        public async void ShouldNotThrow()
         {
+            var client = new Mock<IFormattedHttpClient>();
+            client.Setup(c => c.GetAsync<FacebookFanCountModel>(It.IsAny<string>()))
+                .ReturnsAsync(new FacebookFanCountModel { FanCount = 3 });
 
+            var options = new Mock<IOptions<Facebook>>();
+            options.Setup(o => o.Value).Returns(new Facebook());
+
+            var graph = new FacebookGraph(options.Object, client.Object);
+
+            var value = await graph.GetPageLikeCountAsync();
+
+            Assert.Equal(3, value);
         }
     }
 }
