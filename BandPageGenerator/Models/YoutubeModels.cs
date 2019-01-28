c1095ddf0691154870ff58b8d75aba846a3dd762
csorez <csore.zoltan@gmail.com>
2019-01-29 00:09:48 +0100

    #2 Add view count api request

---
+++ BandPageGenerator/Models/YoutubeModels.cs
@@ -0,0 +1,17 @@
+﻿namespace BandPageGenerator.Models
+{
+    public class YoutubeDataWrapperModel<TModel>
+    {
+        public TModel[] Items { get; set; }
+    }
+
+    public class YoutubeDataModel
+    {
+        public YoutubeStatisticsModel Statistics { get; set; }
+    }
+
+    public class YoutubeStatisticsModel
+    {
+        public long ViewCount { get; set; }
+    }
+}
--- BandPageGenerator/Services/YoutubeClient.cs
+++ BandPageGenerator/Services/YoutubeClient.cs
@@ -1,9 +1,9 @@
 ﻿using BandPageGenerator.Config;
+using BandPageGenerator.Models;
 using BandPageGenerator.Services.Interfaces;
 using Microsoft.Extensions.Options;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace BandPageGenerator.Services
 {
@@ -11,11 +11,41 @@ namespace BandPageGenerator.Services
     {
         private readonly Youtube config;
         private readonly IFormattedHttpClient client;
+        private const string apiUri = "https://www.googleapis.com/youtube/v3/";
 
         public YoutubeClient(IOptions<Youtube> config, IFormattedHttpClient client)
         {
             this.config = config.Value;
             this.client = client;
+        }
+
+        public async Task<long> GetCumulatedViewCount()
+        {
+            var channelViewData = await this.GetApiDataAsync<YoutubeDataWrapperModel<YoutubeDataModel>>(
+                "channel", ("part", "statistics"), ("id", this.config.ChannelId));
+
+            long viewCount = channelViewData.Items[0].Statistics.ViewCount;
+
+            if (this.config.AdditionalVideoIds != null)
+            {
+                var additionalViewData = await this.GetApiDataAsync<YoutubeDataWrapperModel<YoutubeDataModel>>(
+                    "videos", ("part", "statistics"), ("id", string.Join(',', this.config.AdditionalVideoIds)));
+
+                viewCount += additionalViewData.Items.Sum(i => i.Statistics.ViewCount);
+            }
+
+            return viewCount;
+        }
+
+        private Task<TModel> GetApiDataAsync<TModel>(string edge, params (string, string)[] parameters)
+        {
+            var queryString = string.Format("{0}{1}?key={2}{3}",
+                apiUri,
+                edge,
+                this.config.ApiKey,
+                parameters != null ? string.Join("", parameters.Select(i => $"&{i.Item1}={i.Item2}")) : string.Empty);
+
+            return this.client.GetAsync<TModel>(queryString);
         }
     }
 }
