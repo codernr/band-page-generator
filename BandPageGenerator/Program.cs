f6f420cabe48a198bcc95c86b1cdc4fdf5fb41d0
codernr <zoltan.csore@codernr.com>
2019-01-31 00:48:48 +0100

    #5 Make services defined only if config is present

--- BandPageGenerator/Program.cs
+++ BandPageGenerator/Program.cs
@@ -1,13 +1,7 @@
-﻿using BandPageGenerator.Config;
-using BandPageGenerator.Services;
+﻿using BandPageGenerator.Services;
 using BandPageGenerator.Services.Interfaces;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
-using Stubble.Core.Builders;
-using Stubble.Core.Interfaces;
 using System;
 using System.IO;
 
@@ -17,7 +11,7 @@ namespace BandPageGenerator
     {
         static void Main(string[] args)
         {
-            var serviceProvider = ConfigureServices().BuildServiceProvider();
+            var serviceProvider = ServiceConfiguration.ConfigureServiceProvider();
 
             var logger = serviceProvider.GetService<ILoggerFactory>().CreateLogger<Program>();
 
@@ -38,33 +32,6 @@ namespace BandPageGenerator
             logger.LogInformation("Count: " + fbTask.Result);
 
             Console.ReadKey();
-        }
-
-        static IServiceCollection ConfigureServices()
-        {
-            var serviceCollection = new ServiceCollection()
-                .AddLogging(logging => logging.AddConsole())
-                .AddSingleton<IAsyncStubbleRenderer>(new StubbleBuilder().Build())
-                .AddSingleton<IViewRenderer, StubbleViewRenderer>();
-
-            serviceCollection.AddHttpClient<IJsonHttpClient<SnakeCaseNamingStrategy>, JsonHttpClient<SnakeCaseNamingStrategy>>();
-            serviceCollection.AddHttpClient<IJsonHttpClient<CamelCaseNamingStrategy>, JsonHttpClient<CamelCaseNamingStrategy>>();
-
-            var configuration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", false)
-                .Build();
-            serviceCollection.AddOptions();
-            serviceCollection.Configure<Facebook>(configuration.GetSection("Facebook"));
-            serviceCollection.Configure<Youtube>(configuration.GetSection("Youtube"));
-            serviceCollection.Configure<Spotify>(configuration.GetSection("Spotify"));
-
-            serviceCollection
-                .AddSingleton<FacebookClient>()
-                .AddSingleton<YoutubeClient>()
-                .AddSingleton<SpotifyClient>();
-
-            return serviceCollection;
         }
     }
 }
---
+++ BandPageGenerator/ServiceConfiguration.cs
@@ -0,0 +1,65 @@
+﻿using BandPageGenerator.Config;
+using BandPageGenerator.Services;
+using BandPageGenerator.Services.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
+using Stubble.Core.Builders;
+using Stubble.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace BandPageGenerator
+{
+    public static class ServiceConfiguration
+    {
+        private static readonly Dictionary<string, Action<IServiceCollection, IConfigurationRoot>> modules =
+            new Dictionary<string, Action<IServiceCollection, IConfigurationRoot>>
+            {
+                { "Facebook", ConfigureFacebook },
+                { "Youtube", ConfigureYoutube },
+                { "Spotify", ConfigureSpotify }
+            };
+
+        public static IServiceProvider ConfigureServiceProvider()
+        {
+            var serviceCollection = new ServiceCollection()
+                .AddLogging(logging => logging.AddConsole())
+                .AddSingleton<IAsyncStubbleRenderer>(new StubbleBuilder().Build())
+                .AddSingleton<IViewRenderer, StubbleViewRenderer>();
+
+            serviceCollection.AddHttpClient<IJsonHttpClient<SnakeCaseNamingStrategy>, JsonHttpClient<SnakeCaseNamingStrategy>>();
+            serviceCollection.AddHttpClient<IJsonHttpClient<CamelCaseNamingStrategy>, JsonHttpClient<CamelCaseNamingStrategy>>();
+
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+            serviceCollection.AddOptions();
+
+            foreach (var section in configuration.GetChildren()) modules[section.Key](serviceCollection, configuration);
+
+            return serviceCollection.BuildServiceProvider();
+        }
+
+        private static void ConfigureFacebook(IServiceCollection serviceCollection, IConfigurationRoot configuration)
+        {
+            serviceCollection.Configure<Facebook>(configuration.GetSection("Facebook"));
+            serviceCollection.AddSingleton<FacebookClient>();
+        }
+
+        private static void ConfigureYoutube(IServiceCollection serviceCollection, IConfigurationRoot configuration)
+        {
+            serviceCollection.Configure<Youtube>(configuration.GetSection("Youtube"));
+            serviceCollection.AddSingleton<YoutubeClient>();
+        }
+
+        private static void ConfigureSpotify(IServiceCollection serviceCollection, IConfigurationRoot configuration)
+        {
+            serviceCollection.Configure<Spotify>(configuration.GetSection("Spotify"));
+            serviceCollection.AddSingleton<SpotifyClient>();
+        }
+    }
+}
