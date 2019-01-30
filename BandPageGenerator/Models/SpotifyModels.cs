b1c7e7fed3bfb9e22ab17ac102ef20958b7c98be
codernr <zoltan.csore@codernr.com>
2019-01-30 17:45:38 +0100

    #4 Add Spotify models

--- BandPageGenerator/Models/SpotifyModels.cs
+++ BandPageGenerator/Models/SpotifyModels.cs
@@ -8,4 +8,55 @@
 
         public int ExpiresIn { get; set; }
     }
+
+    public class SpotifyPagingModel<TModel>
+    {
+        public TModel[] Items { get; set; }
+    }
+
+    public class SpotifySimplifiedAlbumModel
+    {
+        public string Href { get; set; }
+    }
+
+    public class SpotifyAlbumModel
+    {
+        public string AlbumType { get; set; }
+
+        public string Id { get; set; }
+
+        public SpotifyImageModel[] Images { get; set; }
+
+        public string Label { get; set; }
+
+        public string Name { get; set; }
+
+        public string ReleaseDate { get; set; }
+
+        public string ReleaseDatePrecision { get; set; }
+
+        public SpotifyPagingModel<SpotifyTrackModel> Tracks { get; set; }
+
+        public string Type;
+    }
+
+    public class SpotifyImageModel
+    {
+        public int Width { get; set; }
+
+        public int Height { get; set; }
+
+        public int Url { get; set; }
+    }
+
+    public class SpotifyTrackModel
+    {
+        public int DurationMs { get; set; }
+
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int TrackNumber { get; set; }
+    }
 }
