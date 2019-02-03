f2735bb7f728f47550d790f48785dcb6e092b090
codernr <zoltan.csore@codernr.com>
2019-02-03 23:46:52 +0100

    Fix variable include

--- release.sh
+++ release.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-upload_url=$(curl -s -H "Authorization: token $GITHUB_RELEASE_TOKEN" -d '{"tag_name": "$CI_COMMIT_TAG", "name":"$CI_COMMIT_TAG","body":"Automatic release", "draft": true}' "https://api.github.com/repos/codernr/band-page-generator/releases")
+body='{"tag_name": "%s", "name":"%","body":"Automatic release", "draft": true}'
+
+printf -v formatted_body $body $CI_COMMIT_TAG $CI_COMMIT_TAG
+
+upload_url=$(curl -s -H "Authorization: token $GITHUB_RELEASE_TOKEN" -d $formatted_body "https://api.github.com/repos/codernr/band-page-generator/releases")
 
 upload_url=$(egrep -oh -m 1 "(https://uploads.github.com/repos/codernr/band-page-generator/releases/[0-9]+/assets)" <<< $upload_url)
 
