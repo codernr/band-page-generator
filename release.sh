87f605df4c5e221cea47b3f8a85f5e795aa70ae7
codernr <zoltan.csore@codernr.com>
2019-02-03 23:34:45 +0100

    Fix release script

--- release.sh
+++ release.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-GITHUB_RELEASE_TOKEN=b496b41a5bf7f0b87e9c6803f9255e9cb133728c
-CI_COMMIT_TAG=terst-ci-deploy-6
-
 upload_url=$(curl -s -H "Authorization: token $GITHUB_RELEASE_TOKEN" -d '{"tag_name": "$CI_COMMIT_TAG", "name":"$CI_COMMIT_TAG","body":"Automatic release", "draft": true}' "https://api.github.com/repos/codernr/band-page-generator/releases")
 
 upload_url=$(egrep -oh -m 1 "(https://uploads.github.com/repos/codernr/band-page-generator/releases/[0-9]+/assets)" <<< $upload_url)
