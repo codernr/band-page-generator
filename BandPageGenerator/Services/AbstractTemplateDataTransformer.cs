69cf7dcbe1f7be2d484b1f26a774548fe72eea9f
codernr <zoltan.csore@codernr.com>
2019-02-18 19:54:56 +0100

    #10 Add expression based download url replacement to an abstract base class

---
+++ BandPageGenerator/Services/AbstractTemplateDataTransformer.cs
@@ -0,0 +1,54 @@
+﻿using BandPageGenerator.Config;
+using BandPageGenerator.Services.Interfaces;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace BandPageGenerator.Services
+{
+    public abstract class AbstractTemplateDataTransformer : ITemplateDataTransformer
+    {
+        protected readonly DownloaderClient downloader;
+        protected readonly GeneralConfig generalConfig;
+
+        public AbstractTemplateDataTransformer(DownloaderClient downloader, IOptions<GeneralConfig> config)
+        {
+            this.downloader = downloader;
+            this.generalConfig = config.Value;
+        }
+
+        public abstract Task AddTemplateDataAsync(Dictionary<string, object> templateData);
+
+        protected async Task<IEnumerable<T>> Replace<T>(IEnumerable<T> data, Expression<Func<T, string>> memberLambda, Expression<Func<T, string>> idLambda)
+        {
+            var urlGetter = memberLambda.Compile();
+            var idGetter = idLambda.Compile();
+
+            var member = (MemberExpression)memberLambda.Body;
+            var param = Expression.Parameter(typeof(string));
+            var urlSetter = Expression.Lambda<Action<T, string>>(
+                Expression.Assign(member, param), memberLambda.Parameters[0], param).Compile();
+
+            var tasks = data.Select(async target =>
+            {
+                var url = memberLambda.Compile()(target);
+
+                var id = idLambda.Compile()(target);
+
+                var newValue = await this.downloader.DownloadFile(
+                            url, id, this.generalConfig.DownloadSavePath, this.generalConfig.DownloadedBasePath);
+
+                urlSetter(target, newValue);
+
+                return target;
+            }).ToArray();
+
+            await Task.WhenAll(tasks);
+
+            return tasks.Select(t => t.Result);
+        }
+    }
+}
--- BandPageGenerator/Services/YoutubeTemplateDataTransformer.cs
+++ BandPageGenerator/Services/YoutubeTemplateDataTransformer.cs
@@ -11,20 +11,14 @@ using System.Threading.Tasks;
 
 namespace BandPageGenerator.Services
 {
-    public class YoutubeTemplateDataTransformer : ITemplateDataTransformer
+    public class YoutubeTemplateDataTransformer : AbstractTemplateDataTransformer
     {
         private readonly YoutubeClient client;
-        private readonly DownloaderClient downloader;
-        private readonly GeneralConfig generalConfig;
 
         public YoutubeTemplateDataTransformer(YoutubeClient client, DownloaderClient downloader, IOptions<GeneralConfig> generalConfig)
-        {
-            this.client = client;
-            this.downloader = downloader;
-            this.generalConfig = generalConfig.Value;
-        }
+            : base(downloader, generalConfig) => this.client = client;
 
-        public async Task AddTemplateDataAsync(Dictionary<string, object> templateData)
+        public override async Task AddTemplateDataAsync(Dictionary<string, object> templateData)
         {
             templateData.Add("ViewCount", await this.client.GetCumulatedViewCount());
             templateData.Add("Videos", await this.GetVideos());
@@ -35,31 +29,6 @@ namespace BandPageGenerator.Services
             var videoList = await this.client.GetFeaturedVideos();
 
             return (await this.Replace(videoList, v => v.Thumbnail.Url, v => v.Id)).ToList();
-        }
-
-        private async Task<IEnumerable<T>> Replace<T>(IEnumerable<T> data, Expression<Func<T, string>> memberLambda, Expression<Func<T, string>> idLambda)
-        {
-            var tasks = data.Select(async target =>
-            {
-                var memberSelectorExpression = memberLambda.Body as MemberExpression;
-                var idSelectorExpression = idLambda.Body as MemberExpression;
-                if (memberSelectorExpression != null && idSelectorExpression != null)
-                {
-                    var property = memberSelectorExpression.Member as PropertyInfo;
-                    var idProperty = idSelectorExpression.Member as PropertyInfo;
-                    if (property != null)
-                    {
-                        property.SetValue(target, await this.downloader.DownloadFile(
-                            property.GetValue(target) as string, idProperty.GetValue(target) as string,
-                            this.generalConfig.DownloadSavePath, this.generalConfig.DownloadedBasePath), null);
-                    }
-                }
-                return target;
-            });
-
-            await Task.WhenAll(tasks);
-
-            return tasks.Select(t => t.Result);
         }
     }
 }
