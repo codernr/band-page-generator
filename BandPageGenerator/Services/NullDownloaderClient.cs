6fd15370e4e1c969b7afd88dcc9eb5c00ee37d04
codernr <zoltan.csore@codernr.com>
2019-02-19 19:17:32 +0100

    #10 Add NullDownloaderClient

---
+++ BandPageGenerator/Services/NullDownloaderClient.cs
@@ -0,0 +1,13 @@
+﻿using BandPageGenerator.Services.Interfaces;
+using System.Threading.Tasks;
+
+namespace BandPageGenerator.Services
+{
+    public class NullDownloaderClient : IDownloaderClient
+    {
+        public Task<string> DownloadFile(string requestUri, string id, string savePath, string basePath, bool forceDownload = false)
+        {
+            return Task.FromResult(requestUri);
+        }
+    }
+}
