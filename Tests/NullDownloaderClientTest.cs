20afa1fcfe3db01f4575cb38bfecd56160a0a239
codernr <zoltan.csore@codernr.com>
2019-02-20 15:18:50 +0100

    Add NullDownloaderClientTest

---
+++ Tests/NullDownloaderClientTest.cs
@@ -0,0 +1,21 @@
+﻿using BandPageGenerator.Services;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public class NullDownloaderClientTest
+    {
+        [Fact]
+        public static async Task ShouldReturnRequestUri()
+        {
+            var client = new NullDownloaderClient();
+
+            var response0 = await client.DownloadFile("asdf", "x", "z", "t");
+            var response1 = await client.DownloadFile("qwer", "x", "z", "t", true);
+
+            Assert.Equal("asdf", response0);
+            Assert.Equal("qwer", response1);
+        }
+    }
+}
