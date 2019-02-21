6559f843c6dc7d8a69bf3fc23f569e8046514243
codernr <zoltan.csore@codernr.com>
2019-02-21 13:42:00 +0100

    Add SpotifyModelsTest

---
+++ Tests/Models/SpotifyModelsTest.cs
@@ -0,0 +1,22 @@
+﻿using BandPageGenerator.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Tests.Models
+{
+    public class SpotifyModelsTest
+    {
+        [Fact]
+        public static void ShouldConvertMillisecondsToTimeSpan()
+        {
+            var model = new SpotifyTrackModel
+            {
+                DurationMs = 61000
+            };
+
+            Assert.Equal(TimeSpan.FromSeconds(61), model.Duration);
+        }
+    }
+}
