929d89a62161888ea3d839c8c07a1a30c6c0f5fe
codernr <zoltan.csore@codernr.com>
2019-02-21 13:37:57 +0100

    Add FacebookModelsTest

---
+++ Tests/Models/FacebookModelsTest.cs
@@ -0,0 +1,23 @@
+﻿using BandPageGenerator.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests.Models
+{
+    public class FacebookModelsTest
+    {
+        [Fact]
+        public static void ShouldCorrectlyFormatEventDescription()
+        {
+            var model = new FacebookEventModel
+            {
+                Description = "Lineone\nLineTwo\nhttp://www.google.com/?query=asdf"
+            };
+
+            Assert.Equal("Lineone<br/>LineTwo<br/><a href=\"http://www.google.com/?query=asdf\" target=\"_blank\">http://www.google.com/?query=asdf</a>", model.FormattedDescription);
+        }
+    }
+}
