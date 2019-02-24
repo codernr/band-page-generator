8c98d4a5dbc5bde0d5734b6221f1ba344ec82fa9
codernr <zoltan.csore@codernr.com>
2019-02-24 17:24:01 +0100

    Add assertions to SpotifyTemplateDataParserTest

--- Tests/Services/SpotifyTemplateDataTransformerTest.cs
+++ Tests/Services/SpotifyTemplateDataTransformerTest.cs
@@ -74,10 +74,22 @@ namespace Tests.Services
             Assert.True(data.ContainsKey("Albums"));
 
             var albums = (data["Albums"] as IEnumerable<SpotifyAlbumTemplateModel>).ToArray();
+            var album = albums[0];
 
-            Assert.Equal("AlbumName", albums[0].Name = "AlbumName");
-            Assert.Equal(new DateTime(2018, 1, 1), albums[0].ReleaseDate);
-            Assert.Equal("woaaaa", albums[0].Image.Url);
+            Assert.Equal("album", album.AlbumType);
+            Assert.Equal("123", album.Id);
+            Assert.Equal("Label", album.Label);
+            Assert.Equal("Single", album.Type);
+            Assert.Equal("AlbumName", album.Name);
+            Assert.Equal(new DateTime(2018, 1, 1), album.ReleaseDate);
+            Assert.Equal("woaaaa", album.Image.Url);
+            Assert.Equal(10, album.Image.Height);
+            Assert.Equal(20, album.Image.Width);
+            Assert.Equal(20000, album.Tracks[0].DurationMs);
+            Assert.Equal(TimeSpan.FromSeconds(20), album.Tracks[0].Duration);
+            Assert.Equal("track0", album.Tracks[0].Id);
+            Assert.Equal("Track", album.Tracks[0].Name);
+            Assert.Equal(1, album.Tracks[0].TrackNumber);
             Assert.Equal("http://asdf.bandcamp.com", albums[0].AlternativeLinks["bandcamp"]);
         }
     }
